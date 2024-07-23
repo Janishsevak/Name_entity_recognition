c080329f265fb33820d12092f637826a7534cfee
Janishsevak <jenishsevak7@gmail.com>
2024-07-23 21:23:08 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Janishsevak",
+    author_email="jenishsevak7@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
